c0d754298fcc60523aae0c99293083cee31b11bc
yunghuey <94943587+yunghuey@users.noreply.github.com>
2024-05-26 08:11:55 +0800

    Create devil_group.cpp

---
+++ yunghuey/devil_group.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    string s;
+    cin>>s;
+    int a=0,mx=0;
+    for(int i=0;i< s.length();i++)
+    {
+        a++;
+        if(s[i]=='@'||s[i]=='$')
+            {mx=max(a,mx);a=0;}
+    }
+    cout<< mx;
+}
